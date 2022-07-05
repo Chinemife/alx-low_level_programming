85895231a3ef2845aba4b552439d17ac25ec5f33
Chinemife <chinemife@gmail.com>
2022-07-05 11:06:49 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - printing _putchar using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+
+	return (0);
+}
