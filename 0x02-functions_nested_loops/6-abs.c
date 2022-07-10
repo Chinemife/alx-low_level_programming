a77a7251c25c8ee0bc631745e7c31074f11f532c
Chinemife <chinemife@gmail.com>
2022-07-10 20:38:00 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute figures
+ * @r: enters the required number
+ * Return: r if positive or -r if negative
+ */
+
+int _abs(int r)
+{
+if (r >= 0)
+{	
+	return (r);
+}
+else
+{
+	return (-r);
+}
+}
