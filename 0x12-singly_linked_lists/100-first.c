07d5004c53e0a0d5ba855b96407307a555033d9b
Chinemife <chinemife@gmail.com>
2022-08-12 13:23:09 +0000

    task5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,6 +3,8 @@
  * before_main - A function that executes before main
  * Return: always void
  */
+void before_main(void) __attribute__ ((constructor));
+
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
